f74a7a7b7bb283c42c7b6cebfaf2cd0045119e9d
trumpetcoder <kevindjames55@gmail.com>
2018-01-13 12:44:29 -0700

    Added dependencies for passport

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 // Requiring express in the application
-var express = require('express');
+var express = require('express'),
+	bodyParser = require('body-parser'),
+    app = express(),
+    mongoose = require('mongoose');
 
-var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 var port = process.env.PORT || 3000;
 
@@ -9,16 +13,13 @@ app.get('/', function(req, res) {
 	// Plan is to hit API here returning results with res.send
 
 
-
-
-
-
   res.send("You're Home!");
 });
+
+
 
 // start server
 app.listen(port, function() {
   console.log('Server started on', port); 
 });
 
-// Adding changes to see recommit
