d4904c5d4b5171520a621366d6502e0313fe0991
trumpetcoder <kevindjames55@gmail.com>
2018-01-15 00:17:41 -0700

    Crashed Heroku, still working on localhost

--- server.js
+++ server.js
@@ -39,6 +39,35 @@ app.use(function(req, res, next) {
 // Route reconfig a GET to '/' routes to homeController hits the /views/index.html
 app.get('/', homeController);
 
+// app.get('/api', function api_index(req, res) {
+//   // Documented API endpoints for Den-Brewery-App
+//   res.json({
+//     message: "Welcome to my Brewery api! Here's what you need to know!",
+//     documentation_url: "https://github.com/trumpetcoder/Den-Brew-App/README.md", 
+//     base_url: "https://ancient-fjord-20095.herokuapp.com/", 
+//     endpoints: [
+//       {method: "GET", path: "/api", description: "Describes all available endpoints"},
+//       {method: "GET", path: "/api/profile", description: "A little bit of info about me and my journey"}, // CHANGED
+//       {method: "GET", path: "/api/trumpet", description: "Return a collection of all trumpet players in db"}, // CHANGED
+//       {method: "GET", path: "/api/trumpet/:id", description: "Grab one trumpet player by an id"}, // ADDED
+//       {method: "DELETE", path: "/api/trumpet/:id", description: "Delete one player by id tested in postman"}, // ADDED
+//       {method: "POST", path: "/api/trumpet", description: "Adding a new player to db, tested in postman"}, // ADDED
+//       {method: "PUT", path: "/api/trumpet/:id", description: "Updating a particular value based off an id, this is setup to update the song key"} // ADDED
+//     ]
+//   });
+// });
+
+// app.get('/api/profile', function(req, res) {
+//   // Profile Setup
+//   res.json({
+//     name: 'Kevin James',
+//     city: 'Denver',
+//     github_link: 'https://github.com/trumpetcoder',
+//     description: 'I have an undergraduate degree in Music Ed. and 4+ years of Masters work in music history/performance. I have recently taken on fullstack web development',
+    
+//   });
+// });
+
 // Breweries api index to hit 
 app.get('/api/breweries', function (req, res) { //utilizing hardcoded data
   res.json(breweries); 
@@ -51,5 +80,14 @@ app.get('/api/breweries', function (req, res) { //utilizing hardcoded data
 app.listen(port, function() {
   console.log('Server started on', port);
 });
+
+
+
+
+
+
+
+
+
 
 // End
