32224a20fe9e08b17611d0afb8fb0b6168f177a1
trumpetcoder <kevindjames55@gmail.com>
2018-01-15 22:26:09 -0700

    Started User.js buildout

--- models/user.js
+++ models/user.js
@@ -1,1 +1,25 @@
 // build out getting ready to add passport
+// Requiring mongoose + bcrypt. Setting up built in Schema variable
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+
+// UserSchema to be used for signup/login takes email and password
+var UserSchema = new Schema ({
+	email: String,
+	password: String
+});
+
+//  Setting up the User.Schema with encrypt/ generating a hash with a salt of 8 characters
+UserSchema.methods.encrypt = function(password) {
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+};
+
+// Setting a variable User to the mongoose.model UserSchema
+var User = mongoose.model('User', UserSchema);
+
+// Exporting the variable User set to the above Schema
+module.exports = User;
+
+
+// end
