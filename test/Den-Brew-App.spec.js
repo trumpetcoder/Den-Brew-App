421453dfe5a85e717af7b04a0b040c0a21ebd5b5
trumpetcoder <kevindjames55@gmail.com>
2018-01-16 20:01:32 -0700

    Finished five mocha tests and passed

--- test/Den-Brew-App.spec.js
+++ test/Den-Brew-App.spec.js
@@ -4,11 +4,44 @@ var Brewery = require('../models/breweries'),
 	expect = require('chai').expect;
 
 describe('Checing if Breweries model works', function ()  {
-	describe("new Brewery", function ()  {
-		it("initializes a new Brewery", function ()  {
+	describe("new Brewery name", function ()  {
+		it("initializes a new Brewery name", function ()  {
 			var brewpub = new Brewery();
 			brewpub.name = "Beer Place";
 			expect(brewpub.name).to.equal("Beer Place");
+
+		});
+	});
+});
+
+describe('Checing if Breweries model works', function ()  {
+	describe("new Brewery description", function ()  {
+		it("initializes a new Brewery description", function ()  {
+			var brewpub = new Brewery();
+			brewpub.description = "The greatest Place to drink";
+			expect(brewpub.description).to.equal("The greatest Place to drink");
+
+		});
+	});
+});
+
+describe('Checing if Breweries model website works', function ()  {
+	describe("new Brewery website", function ()  {
+		it("initializes a new Brewery website", function ()  {
+			var brewpub = new Brewery();
+			brewpub.website = "http://brewpub.com";
+			expect(brewpub.website).to.equal("http://brewpub.com");
+
+		});
+	});
+});
+
+describe('Checing if Breweries model established works', function ()  {
+	describe("new Brewery established", function ()  {
+		it("initializes a new Brewery established date", function ()  {
+			var brewpub = new Brewery();
+			brewpub.established = "2018";
+			expect(brewpub.established).to.equal("2018");
 
 		});
 	});
