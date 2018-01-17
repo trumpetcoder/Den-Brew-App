080c81ebabe71cfb087ff81fedd3433333d71a55
trumpetcoder <kevindjames55@gmail.com>
2018-01-16 19:51:29 -0700

    Finished two mocha tests

--- test/Den-Brew-App.spec.js
+++ test/Den-Brew-App.spec.js
@@ -1,22 +1,32 @@
 // Test Directory
-var expect = require('../server'),
-	expect = require('chai');
+var Brewery = require('../models/breweries'),
+	User = require('../models/user'),
+	expect = require('chai').expect;
 
+describe('Checing if Breweries model works', function ()  {
+	describe("new Brewery", function ()  {
+		it("initializes a new Brewery", function ()  {
+			var brewpub = new Brewery();
+			brewpub.name = "Beer Place";
+			expect(brewpub.name).to.equal("Beer Place");
 
-
-
-
-// it('should respond with status 200 - Success', function (done) {
-//        expect(actual_response.statusCode).to.equal(200);
-//        done();
-//      });
-
-describe('server', function () {
-  before(function () {
-    server.listen(3000);
-  });
-
-  after(function () {
-    server.close();
-  });
+		});
+	});
 });
+
+describe('Checing if User email property model works', function ()  {
+	describe("new User email property", function ()  {
+		it("initializes a new User email", function ()  {
+			var userEmail = new User();
+			userEmail.email = "bob@bob.bob";
+			expect(userEmail.email).to.equal("bob@bob.bob");
+
+		});
+	});
+});
+
+
+
+
+
+
